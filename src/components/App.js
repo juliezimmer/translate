1f601b39e1b0158a7c63f134b888420c9fed1c81
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-04-16 21:12:14 -0500

    added UserCreate, Field, and Button components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component{
    state = {
@@ -19,7 +20,7 @@ class App extends React.Component{
                <i className="flag nl" 
                   onClick={() => this.onLanguageChange('dutch')} />
             </div>
-            {this.state.language}
+            <UserCreate />
          </div>
       );
    }
