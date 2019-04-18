a15cc94aef0db0d470601acfe53a4c17e640a96a
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-04-17 19:35:03 -0500

    used the context system in place of redux

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,15 +6,15 @@ class Button extends React.Component {
    // hooks up the context object to the class component
    // static contextType = LanguageContext;
 
-   renderSubmit(value) {
-      return value === 'english' ? 'Submit' : 'Voorleggen';
+   renderSubmit(language) {
+      return language === 'english' ? 'Submit' : 'Voorleggen';
    }
 
       renderButton(color){
          return (
             <button className={`ui button ${color}`}>
                <LanguageContext.Consumer>
-                  {(value) => this.renderSubmit(value)} 
+                  {({ language }) => this.renderSubmit(language)} 
                </LanguageContext.Consumer>
             </button>
          )
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,4 +1,25 @@
 import React from 'react';
 
 // creates the context object
-export default React.createContext('english');
+const Context = React.createContext('english');
+
+// this sets up LanguageStore as a named export.
+export class LanguageStore extends React.Component {
+   state = {
+      language: 'english'
+   };
+
+   // will give other components the ability to change the language selection/state property
+  onLanguageChange = language => {
+      this.setState({ language });
+   }; 
+   
+   render(){
+      return (
+         <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+           {this.props.children} 
+         </Context.Provider>
+      );
+   }
+}
+export default Context;
